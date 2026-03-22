79cca395a454e0916bdeda4622095a29666a88cc
Misael Pereira <misael.pereira.dev@outlook.com>
2026-03-22 16:42:03 +0100

    chore: init project

---
+++ src/main/java/com/misaelpereiradev/persistence_playground/PersistencePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.misaelpereiradev.persistence_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistencePlaygroundApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(PersistencePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misaelpereiradev/persistence_playground/PersistencePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misaelpereiradev.persistence_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistencePlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
